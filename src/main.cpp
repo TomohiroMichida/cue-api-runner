b4e14b73ea98116c6f53869f8b22e2ed4ce23acf
mechtatel <michy0221m@gmail.com>
2025-06-09 23:16:56 +0900

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "cue-api-runner 起動OK" << std::endl;
+  std::cout << "config/requests.ymlを読み込んでバッチ実行します..." << std::endl;
+  return 0;
+}
